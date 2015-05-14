87f62c6746897c60164cc1e0109f32807b9de826
protreptic <bukhal.ps@gmail.com>
2015-05-14 17:03:30 +0400

    Initial commit

---
+++ src/org/javaprotrepticon/android/robotofontset/RobotoFontSet.java
@@ -0,0 +1,13 @@
+package org.javaprotrepticon.android.robotofontset;
+
+import android.content.Context;
+
+public class RobotoFontSet {
+	
+	private Context mContext;
+	
+	public RobotoFontSet(Context context) {
+		
+	}
+	
+}
